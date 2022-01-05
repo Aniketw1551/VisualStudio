994fc88e84b7e50f07f199f8451925c8b2d60e27
Aniketw1551 <aniketwaghmare976@gmail.com>
2022-01-05 13:26:29 +0530

    VS

---
+++ HelloWorld/HelloWorld/Human.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+/// <summary>
+/// creating Human blueprint
+/// </summary>
+ public class Human
+{
+	/// <summary>
+	/// Characteristics of human being
+	/// </summary>
+	string name = "Rahul";
+	double age = 27;
+
+	/// <summary>
+	/// Actions
+	/// </summary>
+	public void Run()
+	{
+		//Human Actions
+		Console.WriteLine("Running,Walking,crawling");
+		Console.ReadLine();
+
+	}
+}
+}
