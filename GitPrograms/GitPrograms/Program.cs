ada55ef152081407ecce3a98bf55cafc1e1f7900
Aniketw1551 <aniketwaghmare976@gmail.com>
2022-01-06 23:59:22 +0530

    Adding Some Git Programs

---
+++ GitPrograms/GitPrograms/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome");
+GitPrograms.ReplaceWord.UserInput();
+GitPrograms.FlipCoin.CoinFlip();
+GitPrograms.PowerofTwo.FindPowerofTwo();
+GitPrograms.HarmonicNumber.PrintHarmonicNumber();
+GitPrograms.Factors.FactorsOfNumber();
