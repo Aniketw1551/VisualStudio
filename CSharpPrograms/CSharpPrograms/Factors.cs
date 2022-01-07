0cd7787efad3ff3d54af3f91b730641886b861e0
Aniketw1551 <aniketwaghmare976@gmail.com>
2022-01-07 23:04:09 +0530

    Adding SwapofTwoNumbers program

--- CSharpPrograms/CSharpPrograms/Factors.cs
+++ CSharpPrograms/CSharpPrograms/Factors.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GitPrograms
+namespace CSharpPrograms
 {
     internal class Factors
     {
--- GitPrograms/GitPrograms/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome");
-GitPrograms.ReplaceWord.UserInput();
-//GitPrograms.FlipCoin.CoinFlip();
-//GitPrograms.PowerofTwo.FindPowerofTwo();
-//GitPrograms.HarmonicNumber.PrintHarmonicNumber();
-//GitPrograms.Factors.FactorsOfNumber();
-Console.ReadLine();
