541a5a8db86dc63510788ab43e99d6145794000b
Aniketw1551 <aniketwaghmare976@gmail.com>
2022-01-07 10:13:38 +0530

    [Aniketw1551] GitPrograms

--- GitPrograms/GitPrograms/Program.cs
+++ GitPrograms/GitPrograms/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome");
 GitPrograms.ReplaceWord.UserInput();
-GitPrograms.FlipCoin.CoinFlip();
-GitPrograms.PowerofTwo.FindPowerofTwo();
-GitPrograms.HarmonicNumber.PrintHarmonicNumber();
-GitPrograms.Factors.FactorsOfNumber();
+//GitPrograms.FlipCoin.CoinFlip();
+//GitPrograms.PowerofTwo.FindPowerofTwo();
+//GitPrograms.HarmonicNumber.PrintHarmonicNumber();
+//GitPrograms.Factors.FactorsOfNumber();
+Console.ReadLine();
